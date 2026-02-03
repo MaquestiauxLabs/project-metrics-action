bfe9d8bcdeed5ff0828314b534bc610d402e10cc
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2026-02-03 13:21:22 +0100

    feat(action): add project metrics breakdown to badge generation

--- scripts/build-badges.sh
+++ scripts/build-badges.sh
@@ -37,6 +37,48 @@ cat > metrics.tmp << EOF
 
 ---
 
+## 📋 Project Breakdown
+
+EOF
+
+# Process each project
+jq -c '.[]' "$PROJECTS_JSON" | while read -r project_json; do
+  project=$(echo "$project_json" | jq -r '.project')
+  todo=$(echo "$project_json" | jq -r '.todo')
+  ongoing=$(echo "$project_json" | jq -r '.ongoing')
+  done=$(echo "$project_json" | jq -r '.done')
+  total=$((todo + ongoing + done))
+  
+  if [[ $total -gt 0 ]]; then
+    completion_pct=$((done * 100 / total))
+  else
+    completion_pct=0
+  fi
+  
+  # Determine project completion color
+  project_color="red"
+  if [[ $completion_pct -ge 80 ]]; then
+    project_color="brightgreen"
+  elif [[ $completion_pct -ge 50 ]]; then
+    project_color="yellow"
+  elif [[ $completion_pct -ge 25 ]]; then
+    project_color="orange"
+  fi
+  
+  # Add project metrics
+  cat >> metrics.tmp << EOF
+### 🚀 $project
+
+![Project Completion](https://img.shields.io/badge/${completion_pct}%25-${project_color}?style=for-the-badge&logo=github&logoColor=white)
+
+![Todo](https://img.shields.io/badge/Todo-$todo-blue?style=flat-square) ![In Progress](https://img.shields.io/badge/In%20Progress-$ongoing-yellow?style=flat-square) ![Done](https://img.shields.io/badge/Done-$done-green?style=flat-square)
+
+EOF
+done
+
+cat >> metrics.tmp << EOF
+---
+
 EOF
 
 echo "_Last updated: $(date -u +'%Y-%m-%d %H:%M UTC')_" >> metrics.tmp
