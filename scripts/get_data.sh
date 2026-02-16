a1ffb6cea9e5760d753e6b8f9e91910f34094947
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2026-02-16 11:56:17 +0100

    feat(get_data): add status field and repository details to project query

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -27,6 +27,32 @@ RAW_RESPONSE=$(gh api graphql -f query='query($org: String!) {
         creator {
           login
         }
+        statusField: field(name: "Status") {
+          ... on ProjectV2SingleSelectField {
+            options {
+              id
+              name
+              color
+            }
+          }
+        }
+        repositories(first: 10) {
+            nodes {
+                id
+                name
+                nameWithOwner
+                url
+                issues {
+                    totalCount
+                }
+                openIssues: issues(states: OPEN) {
+                    totalCount
+                }
+                closedIssues: issues(states: CLOSED) {
+                    totalCount
+                }
+            }
+        }
       }
     }
   }
